477f35f0604b1bb08ff6e53b49b6ab72833cee1e
Young <danceyoung@hotmail.com>
2018-02-08 17:47:14 +0800

    initial project

---
+++ libraries/SelectMultipleButton.js
@@ -0,0 +1,149 @@
+/*
+ * @Author: Young
+ * DSHARP
+ * @flow 
+ * @Date: 2018-02-06 13:54:25 
+ * @Last Modified by: Young
+ * @Last Modified time: 2018-02-07 13:52:31
+ */
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import {
+  View,
+  Text,
+  TouchableWithoutFeedback,
+  Alert,
+  StyleSheet,
+} from 'react-native'
+
+const ios_blue = '#007AFF'
+
+export default class SelectMultipleButton extends Component {
+
+  static propTypes = {
+    selectMultiple: PropTypes.bool,
+
+    selected: PropTypes.bool,
+
+    value: PropTypes.oneOfType(
+      [
+        PropTypes.string,
+        PropTypes.number
+      ]
+    ).isRequired,
+    displayValue: PropTypes.oneOfType(
+      [
+        PropTypes.string,
+        PropTypes.number
+      ]
+    ),
+
+    highLightStyle: PropTypes.shape({
+      borderColor: PropTypes.string.isRequired,
+      backgroundColor: PropTypes.string.isRequired,
+      textColor: PropTypes.string.isRequired,
+      borderTintColor: PropTypes.string.isRequired,
+      backgroundTintColor: PropTypes.string.isRequired,
+      textTintColor: PropTypes.string.isRequired,
+    }),
+
+    buttonViewStyle: PropTypes.object,
+    textStyle: PropTypes.object,
+    singleTap: PropTypes.func,
+
+  }
+
+  static defaultProps = {
+    selected: false,
+    selectMultiple: true,
+    highLightStyle: {
+      borderColor: 'gray',
+      backgroundColor: 'transparent',
+      textColor: 'gray',
+      borderTintColor: ios_blue,
+      backgroundTintColor: 'transparent',
+      textTintColor: ios_blue,
+    },
+
+    singleTap: (valueTap) => { },
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      selected: false,
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      selected: this.props.selected,
+    })
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.selected !== nextProps.selected) {
+      this.setState({
+        selected: nextProps.selected
+      })
+    }
+  }
+
+  render() {
+    return (
+      <TouchableWithoutFeedback
+        onPress={() => {
+          if (this.props.selectMultiple) {
+            this.setState({ selected: !this.state.selected })
+            this.props.singleTap(this.props.value)
+          } else {
+            if (!this.state.selected) {
+              this.setState({ selected: !this.state.selected })
+              this.props.singleTap(this.props.value)
+            }
+          }
+
+        }
+        }>
+        <View
+          style={
+            [
+              styles.button,
+              this.props.buttonViewStyle,
+              {
+                borderColor: this.state.selected ? this.props.highLightStyle.borderTintColor : this.props.highLightStyle.borderColor,
+                backgroundColor: this.state.selected ? this.props.highLightStyle.backgroundTintColor : this.props.highLightStyle.backgroundColor,
+              }
+            ]
+          }>
+          <Text style={
+            [
+              styles.text,
+              this.props.textStyle,
+              { color: this.state.selected ? this.props.highLightStyle.textTintColor : this.props.highLightStyle.textColor }
+            ]
+          }>
+            {this.props.displayValue === undefined ? this.props.value : this.props.displayValue}
+          </Text>
+        </View>
+      </TouchableWithoutFeedback >
+    )
+  }
+}
+
+styles = StyleSheet.create({
+  button: {
+    margin: 5,
+    borderRadius: 3,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1
+  },
+  text: {
+    textAlign: 'center',
+    marginTop: 5,
+    marginBottom: 5,
+    marginLeft: 10,
+    marginRight: 10,
+  }
+})
---
+++ libraries/SelectMultipleGroupButton.js
@@ -0,0 +1,137 @@
+/*
+ * @Author: Young
+ * DSHARP
+ * @flow 
+ * @Date: 2018-02-07 14:08:34 
+ * @Last Modified by: Young
+ * @Last Modified time: 2018-02-07 15:54:17
+ */
+import React, { Component } from 'react'
+import {
+  View,
+  Text,
+  StyleSheet
+} from 'react-native'
+
+import PropTypes from 'prop-types'
+import _ from 'lodash'
+import SelectMultipleButton from './SelectMultipleButton'
+
+const ios_blue = '#007AFF'
+
+export default class SelectMultipleGroupButton extends Component {
+
+  static propTypes = {
+    selectMultiple: PropTypes.bool,
+
+    group: PropTypes.arrayOf(PropTypes.shape({
+      value: PropTypes.oneOfType(
+        [
+          PropTypes.string,
+          PropTypes.number
+        ]
+      ).isRequired,
+      displayValue: PropTypes.oneOfType(
+        [
+          PropTypes.string,
+          PropTypes.number
+        ]
+      ),
+    })).isRequired,
+
+    highLightStyle: PropTypes.shape({
+      borderColor: PropTypes.string.isRequired,
+      backgroundColor: PropTypes.string.isRequired,
+      textColor: PropTypes.string.isRequired,
+      borderTintColor: PropTypes.string.isRequired,
+      backgroundTintColor: PropTypes.string.isRequired,
+      textTintColor: PropTypes.string.isRequired,
+    }),
+    containerViewStyle: PropTypes.object,
+    buttonViewStyle: PropTypes.object,
+    textStyle: PropTypes.object,
+
+    singleTap: PropTypes.func,
+    onSelectedValuesChange: PropTypes.func,
+  }
+
+  static defaultProps = {
+    selectMultiple: true,
+    singleTap: (valueTap) => { },
+    onSelectedValuesChange: (selectedValues) => { }
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      multipleSelectedData: [],
+      radioSelectedData: ''
+    }
+  }
+
+  _singleTapMultipleSelectedButtons(valueTap) {
+    if (this.props.selectMultiple) {
+      if (this.state.multipleSelectedData.includes(valueTap)) {
+        _.remove(this.state.multipleSelectedData, (item) => { return item === valueTap })
+      } else {
+        this.state.multipleSelectedData.push(valueTap)
+      }
+
+
+      this.props.onSelectedValuesChange(this.state.multipleSelectedData)
+
+      this.setState(
+        {
+          multipleSelectedData: this.state.multipleSelectedData
+        }
+      )
+    } else {
+      this.setState(
+        {
+          radioSelectedData: valueTap
+        }
+      )
+    }
+
+    this.props.singleTap(valueTap)
+
+  }
+
+  _selectedStatus(value) {
+    if (this.props.selectMultiple) {
+      return this.state.multipleSelectedData.includes(value)
+    } else {
+      return this.state.radioSelectedData === value
+    }
+  }
+
+  render() {
+    return (
+      <View
+        style={
+          [
+            {
+              flexWrap: 'wrap', flexDirection: 'row', justifyContent: 'center'
+            },
+            this.props.containerViewStyle
+          ]
+        }>
+        {
+          this.props.group.map((ele, index) =>
+            <SelectMultipleButton
+              key={ele.value + index}
+              buttonViewStyle={this.props.buttonViewStyle}
+              textStyle={this.props.textStyle}
+              highLightStyle={this.props.highLightStyle}
+              selectMultiple={this.props.selectMultiple}
+              value={ele.value}
+              displayValue={ele.displayValue}
+              selected={this._selectedStatus(ele.value)}
+              singleTap={(valueTap) => this._singleTapMultipleSelectedButtons(valueTap)} />
+          )
+        }
+      </View>
+    )
+  }
+}
