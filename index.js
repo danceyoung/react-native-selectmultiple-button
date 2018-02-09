99d04a0f36e811e1fb6c1da951891e348f5f523e
Young <danceyoung@hotmail.com>
2018-02-09 14:17:52 +0800

    export all in index

--- index.js
+++ index.js
@@ -4,11 +4,11 @@
  * @flow 
  * @Date: 2018-02-08 17:52:34 
  * @Last Modified by: Young
- * @Last Modified time: 2018-02-08 17:52:55
+ * @Last Modified time: 2018-02-09 10:15:13
  */
 
 import SelectMultipleButton from './libraries/SelectMultipleButton'
-import SelectMultipleGroupButton from './SelectMultipleGroupButton'
+import SelectMultipleGroupButton from './libraries/SelectMultipleGroupButton'
 
 module.exports = {
   SelectMultipleButton,
